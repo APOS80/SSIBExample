78f7496e62f45bf47e9b4042da694b6318a9fc4a
Andreas Olsson <photoguy.apo@gmail.com>
2016-03-23 07:34:56 +0100

    Added files via upload

---
+++ gui2/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gui2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ gui2/project.clj
@@ -0,0 +1,10 @@
+(defproject gui2 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [seesaw "1.4.5"]]
+  :main ^:skip-aot gui2.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
